bfa4fa9b74c11925b85f8eb22f4c3cde32328fba
Vijay-Kannan-1405 <vjkannan55@gmail.com>
2021-07-08 18:36:35 +0530

    Updated

--- heartblink.py
+++ heartblink.py
@@ -5,7 +5,7 @@ import cursor
 
 cursor.hide()
 
-a = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+a = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twele']
 b = ['red', 'green', 'yellow', 'blue']
 
 def one(x):
@@ -15,38 +15,53 @@ def one(x):
     print(' ')
     print(' ')
     print(' ')
-    print(" "*5, colored("*"*4, color), " "*3, colored("*"*4, color))
+    print(' ')
+    print(' ')
+    print(' ')
+    print(' ')
+    print(' ')
+    print(' ')
+    print(" "*42, colored("*"*8, color), " "*3, colored("*"*8, color))
 
 def two(y):
     color = str(y)
-    print(" "*4, colored("*"*6, color), " "*1, colored("*"*6, color))
+    print(" "*39, colored("*"*12, color), " "*1, colored("*"*12, color))
 
 def three(z):
     color = str(z)
-    print(" "*5, colored("*"*13, color))
+    print(" "*38, colored("*"*29, color))
 
 def four(p):
     color = str(p)
-    print(" "*6, colored("*"*11, color))
+    print(" "*39, colored("*"*27, color))
 
 def five(q):
     color = str(q)
-    print(" "*7, colored("*"*9, color))
+    print(" "*40, colored("*"*25, color))
 
 def six(r):
     color = str(r)
-    print(" "*8,colored( "*"*7, color))
+    print(" "*41,colored( "*"*23, color))
 
 def seven(s):
     color = str(s)
-    print(" "*9, colored("*"*5, color))
+    print(" "*43, colored("*"*19, color))
 
 def eight(w):
     color = str(w)
-    print(" "*10, colored("*"*3, color))
+    print(" "*45, colored("*"*15, color))
 def nine(n):
     color = str(n)
-    print(" "*11, colored("*"*1, color))
+    print(" "*47, colored("*"*11, color))
+def ten(n):
+    color = str(n)
+    print(" "*49, colored("*"*7, color))
+def eleven(n):
+    color = str(n)
+    print(" "*51, colored("*"*3, color))
+def twele(n):
+    color = str(n)
+    print(" "*52, colored("*"*1, color))
 
 # this is main method
 
